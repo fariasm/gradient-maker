a7bb85e9221bfd823c4b50466d760d39e3e0cea7
fariasm <federicoariasm@gmail.com>
2022-04-10 19:49:04 -0300

    Modules and components creation.

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./templates/templates.module').then( m => m.TemplatesModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app/src/app/templates/templates.module.ts
+++ app/src/app/templates/templates.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TemplatesRoutingModule } from './templates-routing.module';
+import { MainComponent } from './pages/main/main.component';
+import { TemplatesListComponent } from './components/templates-list/templates-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainComponent,
+    TemplatesListComponent
+  ],
   imports: [
     CommonModule,
     TemplatesRoutingModule
