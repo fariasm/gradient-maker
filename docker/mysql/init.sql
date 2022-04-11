a7a6f0a7cf4ce77148fbfbeeae8f9134f2e4f548
fariasm <federicoariasm@gmail.com>
2022-04-11 11:42:17 -0300

    Changes to init.sql

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -1,2 +1,40 @@
 CREATE DATABASE IF NOT EXISTS `gradient_maker_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `gradient_maker_db`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `templates`
+--
+
+CREATE TABLE `templates` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `style` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `direction` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `color_from` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `color_to` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `templates`
+--
+ALTER TABLE `templates`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `templates`
+--
+ALTER TABLE `templates`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
