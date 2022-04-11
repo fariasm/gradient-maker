3a80cd264037702fe23e92645a98edbfec5660d8
fariasm <federicoariasm@gmail.com>
2022-04-11 14:12:18 -0300

    Fix api example test.

--- api/tests/Unit/ExampleTest.php
+++ api/tests/Unit/ExampleTest.php
@@ -6,5 +6,9 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    
+    public function test_basic()
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200);
+    }
 }
